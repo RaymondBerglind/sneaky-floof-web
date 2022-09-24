6ef0f4f7c551dd8ba9d754c7ce68890f495e053a
Raymond Berglind <raymond.berglind@gmail.com>
2022-09-25 01:10:38 +0200

    Clean up stylesheet.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="logo" alt="logo" />
     </div>
   );
 }
